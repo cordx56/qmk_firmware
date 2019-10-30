4fa91eee562ff81746b9e4c37e2d060a6f183a00
Kaoru Chisen <cordx56@cordx.net>
2019-10-31 02:02:57 +0900

    display total hour/min/sec time

--- keyboards/crkbd/lib/timelogger.c
+++ keyboards/crkbd/lib/timelogger.c
@@ -31,7 +31,10 @@ void check_timer_reset(void) {
 
 void set_totaltime(void) {
   total_time = (unsigned long)0xffff * reset_count + last_time;
-  snprintf(totaltime_str, sizeof(totaltime_str), "total: %7lus", total_time / 1000);
+  unsigned int hour = total_time / 3600000;
+  unsigned int min = (total_time / 60000) % 60;
+  unsigned int sec = (total_time / 1000) % 60;
+  snprintf(totaltime_str, sizeof(totaltime_str), "%4uh %2um %2us,%6lu", hour, min, sec, total_time / 1000);
 }
 const char *read_totaltime(void) {
   check_timer_reset();
