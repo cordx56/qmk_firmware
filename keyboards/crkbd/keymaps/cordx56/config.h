a494f0fc95be13c348337c2e5333102fdb23fa2d
Kaoru Chisen <cordx56@cordx.net>
2020-07-20 03:52:03 +0900

    LED support for crkbd

--- keyboards/crkbd/keymaps/cordx56/config.h
+++ keyboards/crkbd/keymaps/cordx56/config.h
@@ -43,4 +43,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     #define RGBLIGHT_HUE_STEP 10
     #define RGBLIGHT_SAT_STEP 17
     #define RGBLIGHT_VAL_STEP 17
+    #define BACKLIGHT_PIN B7
 #endif
